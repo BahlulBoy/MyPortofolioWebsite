cd8ecd2c36d67c7384d0b3c4792beaa0d16b06ca
BahlulBoy <bahlulsukses@gmail.com>
2023-08-03 01:22:34 +0700

    --

--- style/script.js
+++ style/script.js
@@ -1,12 +1,30 @@
 // scroll nav function
-var navItem = document.querySelectorAll('.nav-action a')
+const navItem = document.querySelectorAll('.nav-item a')
 navItem.forEach(function (link) {
     link.addEventListener('click', function(e) {
         e.preventDefault();
         var targetId = link.getAttribute('href')
+        document.querySelector(targetId).classList.add('show');
         document.querySelector(targetId).scrollIntoView({
-            behavior : 'smooth'
+            behavior : 'smooth',
+            inline : 'start'
         })
     })
 })
 // scroll nav function
+
+const content = document.querySelectorAll('section');
+window.addEventListener('scroll', checkContent);
+
+checkContent();
+
+function checkContent(params) {
+  const triggerBottom = window.innerHeight / 5 * 4;
+  console.log(triggerBottom);
+  content.forEach(b => {
+    const contentItem = b.getBoundingClientRect().top
+    if (contentItem < triggerBottom) {
+        b.classList.add('show');
+    }
+  })
+};
